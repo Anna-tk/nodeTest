1db998c9944a92ed65d3a425344241711e3c6218
Tkachenko_Ann <summer.199@mail.ru>
2021-10-14 21:07:00 +0300

    nodejs type

--- topAPI/test.ts
+++ topAPI/test.ts
@@ -24,4 +24,47 @@ enum Test1 {
 
 const test = Test1.A;
 const nameA = Test1[test];
-console.log(nameA)
+console.log(nameA);
+
+//type
+
+type Point = {
+    x: number;
+    y: number;
+}
+type p = keyof Point; // union типа x, y
+function MyF() {
+    return {a : 1}
+}
+type K = ReturnType<typeof MyF>
+
+const MyArray = [{
+    name: 'Ann',
+    age: 25
+}]
+type Person = typeof MyArray[number];
+type Age = typeof MyArray[number]['age'];
+
+type MessageOf<T> = T extends {message : unknown} ? T['message'] : never;
+interface Email {
+    message: string;
+}
+interface Cat {
+    test: number;
+}
+
+type EmailMessageContents = MessageOf<Email>
+type CatMessageContents = MessageOf<Cat>
+
+// неограниченное кол-во ключей и значений
+interface Test {
+    [key: string]: number;
+}
+// мапить один тип на другой
+type OptionFlags<Type> ={
+    [Property in keyof Type]: boolean;
+}
+
+// литеральные типы
+type world = 'world'
+type Greeting = `hello ${world}`
